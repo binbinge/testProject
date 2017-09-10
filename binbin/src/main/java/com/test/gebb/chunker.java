055dba636e4f227c5cc182dbd473ad8553b0fb1f
binbinge <390496217@qq.com>
2017-09-10 22:10:25 +0800

    add all

---
+++ binbin/src/main/java/com/test/gebb/chunker.java
@@ -0,0 +1,14 @@
+package com.test.gebb;
+
+public class chunker {
+
+    public void hasValue() {
+        int a = 1;
+        String str = "ABC";
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ binbin/src/main/java/com/test/gebb/test.java
@@ -0,0 +1,18 @@
+package com.test.gebb;
+
+public class test {
+
+    public void hasValue() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+
+    }
+
+}
